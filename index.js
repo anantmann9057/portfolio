143b1274566554738143673615d9699714e6dd9d
Anant Kumar Mann <37663597+anantmann9057@users.noreply.github.com>
2024-12-02 14:32:18 +0530

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.ejs');
+});
+
+app.get("/projects",(req,res)=>{
+    res.render('projects.ejs');
+});
+
+app.listen(port,()=>{
+ console.log(`sever started at ${port}`)
+});
+
